39d78ba1a508f1ae84158545268c33a72dad1de3
JonathWesley <jonathherdt@gmail.com>
2020-09-14 16:39:52 -0300

    aula03

--- Aula03/mediaMovel.py
+++ Aula03/mediaMovel.py
@@ -8,15 +8,17 @@ def moving_average(a, n) :
     return result[n - 1:]
 
 if __name__ == "__main__":
-    with open ('wn.pcm', 'rb') as f:
-        buffer = f.read ()
-        data = np.frombuffer (buffer, dtype = 'int16')
-        left = data [:: 2]
-        right = data [1 :: 2]
+    #with open ('degrauUnitario.pcm', 'rb') as f:
+    #    buffer = f.read ()
+    #    data = np.frombuffer (buffer, dtype = 'int16')
+    #    left = data [:: 2]
+    #    right = data [1 :: 2]
         
-    keys = [4, 8, 16, 32, 64, 128, 256, 512, 1024]  
+    #keys = [4, 8, 16, 32, 64, 128, 256, 512, 1024]  
     
-    size = int(input("Digite o tamanho (exponencial de 2): "))
+    size = 8#int(input("Digite o tamanho (exponencial de 2): "))
+    
+    data = [1, 0.5, 0.25, 0.125]
     
     result = moving_average(data, size)
     plt.figure(figsize=[15, 10])
@@ -25,7 +27,7 @@ if __name__ == "__main__":
     plt.legend(loc = 2)
     
     
-    with open("mediaMovel_wn.pcm", "wb") as new_file:
+    with open("mediaMovel_vetor.pcm", "wb") as new_file:
         for x in result:
             new_file.write(x)
     new_file.close()
